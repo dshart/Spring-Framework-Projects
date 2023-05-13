289b3a1b6ec8492c49de3378be6f829f2dfc56a8
Dan Hart <dshart@gmail.com>
2023-05-13 11:49:09 -0400

    'restfult-git

---
+++ restful_get/src/main/java/com/example/restservice/RestfulGetApplication.java
@@ -0,0 +1,13 @@
+package com.example.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulGetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulGetApplication.class, args);
+	}
+
+}
---
+++ restful_get/src/test/java/com/example/restful_get/RestfulGetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restful_get;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulGetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
