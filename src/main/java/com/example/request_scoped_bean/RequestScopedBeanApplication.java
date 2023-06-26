af9e7ca691c6cf50922f2eb023067e3a4d97c7bd
Dan Hart <dshart@gmail.com>
2023-06-25 23:51:25 -0400

    initial commit

---
+++ src/main/java/com/example/request_scoped_bean/RequestScopedBeanApplication.java
@@ -0,0 +1,13 @@
+package com.example.request_scoped_bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestScopedBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestScopedBeanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/request_scoped_bean/RequestScopedBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.request_scoped_bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestScopedBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
